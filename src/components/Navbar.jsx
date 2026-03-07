ecdf651b779001bee6da2b9a683c09f0ea2c9683
Jegatheeswaran Gowtham <jegatheeswarangowtham@gmail.com>
2026-03-07 19:57:16 +0530

    icon update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/home.css";
-import logo from "../assets/logo.png";
+import logo from "../images/logo.png";
 
 export default function Navbar({ onFeedback }) {
   const [open, setOpen] = useState(false);
