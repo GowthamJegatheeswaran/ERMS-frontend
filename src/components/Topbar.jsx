ca67e72b7995be9298d20b30b4186f872147c0d3
Jegatheeswaran Gowtham <jegatheeswarangowtham@gmail.com>
2026-03-08 09:29:29 +0530

    Update Dashboard

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -6,7 +6,7 @@ export default function Topbar({ title }) {
       <div className="topbar-left">
         {/* Direct URL for public folder */}
         <img src="/images/logo.png" alt="Logo" className="topbar-logo" />
-        <span className="topbar-title">{title}</span>
+        <span className="topbar-title">Equipment Request Management System</span>
       </div>
 
       <div className="topbar-right">
